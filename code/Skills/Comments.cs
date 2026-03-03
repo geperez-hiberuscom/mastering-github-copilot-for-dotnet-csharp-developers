81334c0b32d961a09882056379c827d3bc7f1810
geperez-hiberuscom <geperez@hiberus.com>
2026-03-03 22:46:37 +0000

    Agregar clase Comments con método GenerateComment para generar comentarios sobre habilidades

---
+++ code/Skills/Comments.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Skills;
+
+public class Comments
+{
+    // create a method that generates a comment for a given skill
+    public string GenerateComment(string skill)
+    {
+        return $"Great job on your {skill} skill!";
+    }
+}
